173f7f21709bd4a37c8231e32b32942d7d176936
xuchengang <xuchengang@outlook.com>
2020-01-17 11:26:11 +0800

    update results

--- 3_longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.go
+++ 3_longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.go
@@ -1,26 +1,23 @@
 package __longest_substring_without_repeating_characters
 
+import "strings"
+
 func lengthOfLongestSubstring(s string) int {
-	ret := 0
+	l := len(s)
 	left := 0
-	// 保存遍历过的字符的最新下标
-	store := make(map[uint8]int)
+	right := 0
+	res := 0
 
-	max := func(a int, b int) int {
-		if a > b {
-			return a
+	for i := 0; i < l; i++ {
+		if strings.Contains(s[left:right], string(s[i])) {
+			left += strings.Index(s[left:right], string(s[i])) + 1
 		}
-		return b
+		right++
+
+		if res < right-left {
+			res = right - left
+		}
 	}
 
-	for i := 0; i < len(s); i++ {
-		if ci, ok := store[s[i]]; ok { // 如果i字符之前出现过
-			left = max(left, ci)
-		}
-		ret = max(ret, (i - left + 1))
-		// 存储下标时，以1为起始位置，为了保证上面的 left = max(left, ci) 的正确性
-		store[s[i]] = i + 1
-	}
-
-	return ret
+	return res
 }
